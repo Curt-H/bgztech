fe64bc31084617aeb0ea87e1a5e651d4948564cb
FFF000 <guardian.curt@outlook.com>
2019-08-09 11:39:55 +0800

    Feat: User models built

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+from mongoengine import *
+
+connect('bgztech')
+
 
 # Create your models here.
+
+class Users(Document):
+    username = StringField(required=True)
+    password = StringField(required=True)
+    role = StringField(default='user')
+
+
+if __name__ == '__main__':
+    class Test(Document):
+        username = StringField(required=True)
+        password = StringField(required=True)
+
+
+    t = Test()
+    t.username = 'test'
+    t.password = 'test'
+    t.save()
