9b41191510a49c0dffdd482bb2d9ec2a17b7ef8a
FFF000 <guardian.curt@outlook.com>
2019-08-16 11:30:29 +0800

    Feat: get dict data from database

--- dict/views.py
+++ dict/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from dict.models import Dict
 
 
 # Create your views here.
@@ -7,6 +8,8 @@ from django.shortcuts import render
 def dictionary(request):
     context_dict = dict()
 
+    words = [i for i in Dict.objects.all()]
+    context_dict["words"] = words
     return render(request,
                   'blog/dict.html',
                   context=context_dict
