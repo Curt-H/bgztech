6fc470b52f57cefab3c958a69d9bedd9c84e38cb
FFF000 <guardian.curt@outlook.com>
2019-08-16 11:12:47 +0800

    Refactor: use embeddedDocument replace of dictfield

--- dict/models.py
+++ dict/models.py
@@ -4,6 +4,12 @@ from mongoengine import *
 connect('bgztech')
 
 
+class Paraphrase(EmbeddedDocument):
+    part_of_speech = StringField(required=True)
+    paraphrase = StringField(required=True)
+    example = StringField()
+
+
 class Dict(Document):
     word = StringField(required=True, unique=True)
-    paraphrase = ListField(DictField)
+    paraphrase = ListField(EmbeddedDocumentField(Paraphrase))
