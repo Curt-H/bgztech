06eb030201e0bff11758c0219aab3bd34fb24169
FFF000 <guardian.curt@outlook.com>
2019-08-16 11:07:50 +0800

    Feat: dict app's model

--- dict/models.py
+++ dict/models.py
@@ -1,1 +1,9 @@
 # Create your models here.
+from mongoengine import *
+
+connect('bgztech')
+
+
+class Dict(Document):
+    word = StringField(required=True, unique=True)
+    paraphrase = ListField(DictField)
