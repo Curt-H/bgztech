4d6ad5b10721e38deb357c647736dcccc39357ef
FFF000 <guardian.curt@outlook.com>
2019-09-04 23:10:43 +0800

    Feat: todo hompage view and router

--- todolist/views.py
+++ todolist/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # Create your views here.
+from blog import current_user, get_from_cookies
+
+
 def homepage(request):
-    return HttpResponse('homepage')
+    context_dict = dict()
+
+    session_id = get_from_cookies(request, 'session_id')
+    user = current_user(session_id)
+
+    context_dict['user'] = user
+    return render(request,
+                  'todolist/homepage.html',
+                  context=context_dict
+                  )
 
 
 def edit(request):
