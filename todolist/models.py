5c5e3aa93f19303df80f5f81bfc569f37b4ac6b4
FFF000 <guardian.curt@outlook.com>
2019-09-04 20:02:55 +0800

    Update models.py

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from mongoengine import *
+
 
 # Create your models here.
+
+class Todo(document):
+    title = StringField(required=True)
+    content = StringField()
+    tag = ListField(StringField)
+    creat_time = IntField(required=True)
+    expired_time = IntField()
+    repeat = BooleanField()
+    repeat_mark = ListField(IntField)
