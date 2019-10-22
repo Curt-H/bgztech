b6d11dd248e8738b95fc4b0a94581b504b1baa53
FFF000 <guardian.curt@outlook.com>
2019-10-22 23:22:34 +0800

    Feature: add favicon

--- bgztech/urls.py
+++ bgztech/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^favicon.ico$', RedirectView.as_view(url=r'static/img/favicon.ico')),
     url('', include('blog.urls')),
     url('', include('dict.urls')),
     url('', include('todolist.urls')),
