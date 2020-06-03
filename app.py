173ecd7184e27523ffb9b6569a6c37f1b79fd422
Curt_H <guardian.curt@outlook.com>
2020-06-04 00:28:23 +0800

    [Update] add a test router

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/happy')
+def happy():
+    return "Happy!"
 
 if __name__ == '__main__':
     app.run(debug=True, port=80, host="localhost")
