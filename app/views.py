4b0d5763a1544e99722bbd6941af4bbbe51a58dc
FFF000 <guardian.curt@outlook.com>
2020-06-05 11:42:43 +0800

    Refactor: use std dev frame

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from flask import render_template, redirect, url_for
+from flask import Blueprint as bp
+
+main = bp('main', __name__)
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+
+@main.route('/happy')
+def happy():
+    return "Happy!"
+
--- manage.py
+++ manage.py
@@ -5,21 +5,19 @@ from utils import log
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/happy')
-def happy():
-    return "Happy!"
-
-
 def create_app():
     app = Flask(__name__)
 
+    from app.views import main as blueprint_main
+    app.register_blueprint(blueprint_main)
+
+    return app
+
 
 if __name__ == '__main__':
+    app = create_app() 
+
     if app_config['host'] == '0.0.0.0' and app_config['debug']:
         log('Please check page at http://localhost')
+         
     app.run(**app_config)
